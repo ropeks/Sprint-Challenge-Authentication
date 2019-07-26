7174d60547bde726d15abc6d4bd9dd42a77711fd
ropeks <ropeksmilioner@gmail.com>
2019-07-26 12:02:47 +0200

    add tests for GET /api/jokes endpoint

--- config/routes.spec.js
+++ config/routes.spec.js
@@ -1,4 +1,14 @@
 const request = require("supertest");
 const server = require("./../api/server");
-const db = require("./../database/dbConfig");
 
+describe('Server', () => {
+    it('getJokes works', () => {
+        return request(server)
+            .get('/api/jokes')
+            .expect(401)
+            .expect('Content-Type', /json/)
+            .then(res => {
+                expect(res.body).toEqual({ message: 'Please provide token in Authorization header' });
+            });
+    });
+});
